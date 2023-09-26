9d5917421c8b09f1413f47bdec3524c806dd83cb
Dennis Schoepf <dennetix@gmail.com>
2023-09-26 19:04:11 +0200

    Actually opening streams and some refactor

--- src/config.rs
+++ src/config.rs
@@ -6,12 +6,14 @@ use std::{
 use anyhow::{Context, Result};
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
-use tracing::info;
+use serde_json::json;
+use tracing::{error, info};
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct StreamlinkConfig {
     pub streamlink_exec_name: String,
+    pub streamlink_quality: String,
     pub streamlink_args: Vec<String>,
     pub player_exec_name: String,
     pub player_args: Vec<String>,
@@ -21,6 +23,7 @@ impl Default for StreamlinkConfig {
     fn default() -> Self {
         Self {
             streamlink_exec_name: String::from("streamlink"),
+            streamlink_quality: String::from("best"),
             streamlink_args: vec![String::from("--twitch-low-latency")],
             player_exec_name: String::from("mpv"),
             player_args: vec![],
@@ -28,23 +31,17 @@ impl Default for StreamlinkConfig {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct StreamConfig {
     pub name: String,
-    #[serde(default)]
+    #[serde(default = "default_probability")]
     pub probability: f32,
     #[serde(skip)]
     pub online_since: Option<DateTime<Local>>,
 }
 
-impl Default for StreamConfig {
-    fn default() -> Self {
-        Self {
-            name: String::from(""),
-            probability: 1.0,
-            online_since: None,
-        }
-    }
+fn default_probability() -> f32 {
+    1.0
 }
 
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -56,6 +53,8 @@ pub struct Config {
 
 impl Config {
     pub fn load() -> Result<Self> {
+        info!("Loading config...");
+
         let mut config_path =
             dirs::config_dir().context("Couldn't find config directory for platform")?;
         config_path.push("streamlink-twitch-autoplay.json");
@@ -68,15 +67,21 @@ impl Config {
             let config_file = File::open(config_path.as_path())
                 .with_context(|| format!("Failed to open config file {}", config_path.display()))?;
 
-            Ok(serde_json::from_reader(BufReader::new(config_file))
-                .context("Failed to parse config")?)
+            let mut config = serde_json::from_reader::<_, Config>(BufReader::new(config_file))
+                .context("Failed to parse config")?;
+
+            config.update_stream_states()?;
+            Ok(config)
         } else {
             info!("Config file not found. Creating default config.");
 
             let config = Self::default();
 
             let mut config_file = File::create(config_path.as_path()).with_context(|| {
-                format!("Failed to crate config file {}", config_path.display())
+                format!(
+                    "Failed to create default config file {}",
+                    config_path.display()
+                )
             })?;
             config_file
                 .write_all(serde_json::to_string_pretty(&config)?.as_bytes())
@@ -84,5 +89,63 @@ impl Config {
 
             Ok(config)
         }
+    }
+
+    pub fn update_stream_states(&mut self) -> Result<()> {
+        info!("Updating stream states...");
+
+        // Twitch has no public api to query stream state without a client id, so we use the
+        // GraphQL query that their client is using
+        let operations = self.streams.iter().map(|stream| {
+            json!({
+                "operationName": "UseLive",
+                "variables": {
+                    "channelLogin": stream.name
+                },
+                "extensions": {
+                    "persistedQuery": {
+                        "version": 1,
+                        "sha256Hash": "639d5f11bfb8bf3053b424d9ef650d04c4ebb7d94711d644afb08fe9a0fad5d9"
+                    }
+                }
+            })
+        }).collect::<Vec<_>>();
+
+        let body = serde_json::to_string_pretty(&operations)?;
+
+        // The client id used in this request is the same that twitch is using for everybody
+        let client = reqwest::blocking::Client::new();
+        let res = client
+            .post("https://gql.twitch.tv/gql")
+            .header("Client-id", "kimne78kx3ncx6brgo4mv6wki5h1ko")
+            .body(body)
+            .send()
+            .context("Failed to send GraphQL query.")?;
+
+        let results = serde_json::from_str::<Vec<serde_json::Value>>(&res.text()?)
+            .context("Failed to parse GraphQL query response.")?;
+
+        results
+            .iter()
+            .zip(&mut self.streams)
+            .for_each(|(json, stream_config)| {
+                let stream = json
+                    .get("data")
+                    .and_then(|data| data.get("user"))
+                    .and_then(|user| user.get("stream"));
+                if let Some(stream) = stream {
+                    let time_utc = stream
+                        .get("createdAt")
+                        .and_then(|created_at| created_at.as_str())
+                        .and_then(|created_at| DateTime::parse_from_rfc3339(created_at).ok());
+                    if let Some(time_utc) = time_utc {
+                        stream_config.online_since = Some(DateTime::<Local>::from(time_utc));
+                    }
+                } else {
+                    error!("User {} does not exist!", stream_config.name);
+                }
+            });
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::{
+    process::{Child, Command},
+    time::{Duration, Instant},
+};
+
 use anyhow::{Context, Result};
-use config::Config;
-use state::update_stream_states;
+use config::{Config, StreamConfig, StreamlinkConfig};
+use rand::{distributions::WeightedIndex, prelude::Distribution, thread_rng};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 mod config;
-mod state;
+
+struct StreamlinkProcess {
+    stream: StreamConfig,
+    process: Child,
+}
 
 fn main() -> Result<()> {
     tracing::subscriber::set_global_default(
@@ -15,13 +24,101 @@ fn main() -> Result<()> {
     )
     .context("Failed to set default subscriber")?;
 
-    let mut config = Config::load()?;
+    let mut config;
+    let mut rng = thread_rng();
 
-    update_stream_states(&mut config.streams)?;
+    let mut last_updated = Instant::now();
+    let mut streamlink_process: Option<StreamlinkProcess> = None;
 
-    config.streams.iter().for_each(|stream| {
-        info!("{}: {:?}", stream.name, stream.online_since);
-    });
+    loop {
+        // Check if the streamlink process has exited every 5 seconds
+        if let Some(StreamlinkProcess { process, .. }) = &mut streamlink_process {
+            if let Ok(Some(_)) = process.try_wait() {
+                streamlink_process = None;
+            }
+        }
 
-    Ok(())
+        // Update only if no stream is running or every 90 seconds
+        if streamlink_process.is_none() || last_updated.elapsed() > Duration::from_secs(90) {
+            last_updated = Instant::now();
+
+            config = Config::load()?;
+
+            let online_streams = config
+                .streams
+                .iter()
+                .filter(|stream| stream.online_since.is_some())
+                .collect::<Vec<_>>();
+
+            info!(
+                "Online streams: {}",
+                online_streams
+                    .iter()
+                    .map(|stream| stream.name.clone())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+
+            if !online_streams.is_empty() {
+                // Choose the first stream with probability one or higher
+                let mut stream = online_streams
+                    .iter()
+                    .find(|stream| stream.probability >= 1.0)
+                    .cloned();
+
+                // If there is no stream with probability one randomly choose one weighted by probability
+                // Only do this if no stream is currently running to not end up constantly switching streams
+                if stream.is_none() && streamlink_process.is_none() {
+                    let weights = online_streams.iter().map(|stream| stream.probability);
+                    let dist = WeightedIndex::new(weights)?;
+                    stream = Some(online_streams[dist.sample(&mut rng)]);
+                }
+
+                if let Some(stream) = stream {
+                    // If a stream is currently running only stop it if the new one is different
+                    let should_open = if let Some(streamlink_process) = &mut streamlink_process {
+                        if streamlink_process.stream.name != stream.name {
+                            streamlink_process.process.kill()?;
+                            true
+                        } else {
+                            false
+                        }
+                    } else {
+                        true
+                    };
+
+                    if should_open {
+                        streamlink_process = Some(StreamlinkProcess {
+                            stream: stream.clone(),
+                            process: spawn_streamlink_process(
+                                &stream.name,
+                                &config.streamlink_config,
+                            )?,
+                        });
+                    }
+                }
+            }
+        }
+
+        std::thread::sleep(Duration::from_secs(5));
+    }
+}
+
+fn spawn_streamlink_process(name: &str, config: &StreamlinkConfig) -> Result<Child> {
+    let mut command = Command::new(&config.streamlink_exec_name);
+    command
+        .args(&config.streamlink_args)
+        .arg(format!("twitch.tv/{name}"))
+        .arg(&config.streamlink_quality)
+        .args(["--player", "mpv"]);
+
+    if !config.player_args.is_empty() {
+        command.arg("--player-args").args(&config.player_args);
+    }
+
+    info!("Starting streamlink process: {:?}", command);
+
+    command
+        .spawn()
+        .context("Failed to execute streamlink process.")
 }
